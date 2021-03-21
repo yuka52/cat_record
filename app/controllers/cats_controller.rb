fdc1494cf7fbf0eb19801fe304f2bf4d01009d32
yuka52 <clrwtr.710@gmail.com>
2021-03-21 22:57:43 +0900

    猫情報編集機能作成

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -27,6 +27,19 @@ class CatsController < ApplicationController
 
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_path(@cat.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def cat_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cats#index'
-  resources :cats, only: [:new, :create, :show] 
+  resources :cats, only: [:new, :create, :show, :edit, :update] 
   resources :healths, only: [:new, :create, :edit, :update, :destroy]
 
 end
