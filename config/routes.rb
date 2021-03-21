958daf25a823a762b597a3c690b6f820a205b8b4
yuka52 <clrwtr.710@gmail.com>
2021-03-21 22:09:54 +0900

    記録削除機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cats#index'
   resources :cats, only: [:new, :create, :show] 
-  resources :healths, only: [:new, :create, :edit, :update]
+  resources :healths, only: [:new, :create, :edit, :update, :destroy]
 
 end
